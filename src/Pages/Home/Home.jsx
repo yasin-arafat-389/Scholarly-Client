acd7071619596dd23ea646e270acc9380b137a4a
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-03-15 04:48:55 +0600

    Added hero section in home page

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import HeroSection from "../../Components/HeroSection/HeroSection";
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+};
+
+export default Home;
