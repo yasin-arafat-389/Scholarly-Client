4e9483ca435c883a501eb920030ce72e540e3e83
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-03-26 21:34:23 +0600

    Fixed toaster displaying issue

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,11 +1,21 @@
 import { Outlet } from "react-router-dom";
 import ScrollToTop from "../Utilities/ScrollToTop/ScrollToTop";
+import Navbar from "../Components/Navbar/Navbar";
+import { Toaster } from "react-hot-toast";
+import Footer from "../Components/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <div>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{ duration: 5000 }}
+      />
+      <Navbar />
       <ScrollToTop />
       <Outlet />
+      <Footer />
     </div>
   );
 };
