c59f6cff96b6ac1821c9a959eed2609a1460308b
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-03-27 23:44:11 +0600

    Added redux and made some changes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import CreateCoverPage from "../Pages/CreateCoverPage/CreateCoverPage";
+import CreateAssignment from "../Pages/CreateAssignment/CreateAssignment";
+import Home from "../Pages/Home/Home";
 
 const router = createBrowserRouter([
   {
@@ -10,7 +13,15 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <div>Home</div>,
+        element: <Home />,
+      },
+      {
+        path: "/create/cover-page",
+        element: <CreateCoverPage />,
+      },
+      {
+        path: "/create/assignment",
+        element: <CreateAssignment />,
       },
     ],
   },
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
+import "sweetalert2/src/sweetalert2.scss";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
